9c456a9bc38afec3b753b6e3c63eac24637d3751
basavarajakj2000 <basavarajakj06@gmail.com>
2022-11-25 15:26:09 +0530

    Header component done

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+// Icons for Header
+export { SlMenu } from 'react-icons/sl';
+export { IoIosSearch } from 'react-icons/io';
+export { RiVideoAddLine } from 'react-icons/ri';
+export { FiBell } from 'react-icons/fi';
+export { CgClose } from 'react-icons/cg';
